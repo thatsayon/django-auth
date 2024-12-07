305745298679be77e8936f5d177869bdf627fe7e
Ashiqul Islam Ayon <ashiqulislamayon28@gmail.com>
2024-12-07 19:44:49 +0600

    settings bug fix

--- core/settings.py
+++ core/settings.py
@@ -153,7 +153,6 @@ SIMPLE_JWT = {
 
 # email setup
 EMAIL_BACKEND = env('EMAIL_BACKEND')
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = env('EMAIL_HOST')
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
